35b4ae0d922bb37988e074549e01d79c4e404bd2
PhilaNgcamu <philangcamu17@gmail.com>
2024-05-27 14:18:53 +0200

    First Component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import UserRegistrationAndAuthentication from "./components/UserRegistrationAndAuthentication";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UserRegistrationAndAuthentication />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/UserRegistrationAndAuthentication.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const UserRegistrationAndAuthentication = () => {
+  return <Text>User Registration AndAuthentication</Text>;
+};
+
+export default UserRegistrationAndAuthentication;
