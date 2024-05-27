eb86810c6ba76b4a09ee3ef9c8ddf24b6d5532c5
PhilaNgcamu <philangcamu17@gmail.com>
2024-05-27 16:24:16 +0200

    User Signup

--- components/onboarding/UserSignup.js
+++ components/onboarding/UserSignup.js
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
 import {
-  Button,
-  Pressable,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-
 import { useFonts } from "expo-font";
 
-//User registration and authentication
-
+// User registration and authentication
 const UserSignup = ({ navigation }) => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [fontsLoaded, fontError] = useFonts({
+  const [fontsLoaded] = useFonts({
     "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
     "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
   });
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -31,35 +37,45 @@ const UserSignup = ({ navigation }) => {
         app!
       </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Your name"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Your Name</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={setName}
+          keyboardType="default"
+        />
+      </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Your email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Your Email</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Confirm Password</Text>
+        <TextInput
+          style={styles.input}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+      </View>
 
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Create an account</Text>
@@ -87,12 +103,21 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginBottom: 16,
   },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 14,
+    color: "#797C7B",
+    marginBottom: 4,
+  },
   input: {
-    borderWidth: 1,
-    borderColor: "gray",
-    padding: 8,
-    marginBottom: 8,
-    borderRadius: 4,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    paddingBottom: 8,
+    fontFamily: "Poppins-Regular",
+    fontSize: 16,
   },
   buttonText: {
     fontFamily: "Poppins-Bold",
@@ -101,13 +126,10 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
   },
   button: {
-    position: "absolute",
-    bottom: 32,
-    left: 16,
-    right: 16,
     backgroundColor: "#24786D",
     padding: 12,
     borderRadius: 20,
+    marginTop: 16,
   },
   error: {
     color: "red",
