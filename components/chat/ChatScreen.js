ae3a5fc8069814eeeb085a6d30493c1dc6b8c990
PhilaNgcamu <philangcamu17@gmail.com>
2024-05-31 00:43:09 +0200

    Send messages

--- components/chat/ChatScreen.js
+++ components/chat/ChatScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -6,8 +6,6 @@ import {
   TouchableOpacity,
   FlatList,
   StyleSheet,
-  KeyboardAvoidingView,
-  Platform,
 } from "react-native";
 import {
   collection,
@@ -16,111 +14,39 @@ import {
   query,
   where,
   orderBy,
-  onSnapshot,
-  serverTimestamp,
-  updateDoc,
-  doc,
 } from "firebase/firestore";
 import { auth, db } from "../../backend/firebaseConfig";
 import { MaterialIcons } from "@expo/vector-icons";
-import { format } from "date-fns";
 
 const ChatScreen = ({ route }) => {
   const { chatId, chatName } = route.params;
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const [isTyping, setIsTyping] = useState(false);
-  const [otherUserTyping, setOtherUserTyping] = useState(false);
-  const inputRef = useRef(null);
 
   useEffect(() => {
-    const fetchMessages = () => {
+    const fetchMessages = async () => {
       const q = query(
         collection(db, "chats", chatId, "messages"),
         orderBy("createdAt", "asc")
       );
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const messageList = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setMessages(messageList);
-        markMessagesAsRead(messageList);
-      });
-
-      return unsubscribe;
+      const querySnapshot = await getDocs(q);
+      const messageList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMessages(messageList);
     };
-
-    const fetchTypingStatus = () => {
-      const typingDocRef = doc(
-        db,
-        "chats",
-        chatId,
-        "typingStatus",
-        auth.currentUser.uid
-      );
-      const unsubscribe = onSnapshot(typingDocRef, (doc) => {
-        if (doc.exists()) {
-          setOtherUserTyping(doc.data().isTyping);
-        }
-      });
-
-      return unsubscribe;
-    };
-
-    const unsubscribeMessages = fetchMessages();
-    const unsubscribeTyping = fetchTypingStatus();
-
-    return () => {
-      unsubscribeMessages();
-      unsubscribeTyping();
-    };
+    fetchMessages();
   }, [chatId]);
-
-  const markMessagesAsRead = async (messages) => {
-    const unreadMessages = messages.filter(
-      (message) => !message.read && message.userId !== auth.currentUser.uid
-    );
-
-    unreadMessages.forEach(async (message) => {
-      const messageRef = doc(db, "chats", chatId, "messages", message.id);
-      await updateDoc(messageRef, { read: true });
-    });
-  };
 
   const handleSend = async () => {
     if (newMessage.trim() === "") return;
     await addDoc(collection(db, "chats", chatId, "messages"), {
       text: newMessage,
-      createdAt: serverTimestamp(),
+      createdAt: new Date(),
       userId: auth.currentUser.uid,
-      read: false,
     });
     setNewMessage("");
-    setIsTyping(false);
-    await updateTypingStatus(false);
-  };
-
-  const updateTypingStatus = async (status) => {
-    const typingDocRef = doc(
-      db,
-      "chats",
-      chatId,
-      "typingStatus",
-      auth.currentUser.uid
-    );
-    await updateDoc(typingDocRef, { isTyping: status });
-  };
-
-  const handleTyping = (text) => {
-    setNewMessage(text);
-    if (text.trim() !== "" && !isTyping) {
-      setIsTyping(true);
-      updateTypingStatus(true);
-    } else if (text.trim() === "" && isTyping) {
-      setIsTyping(false);
-      updateTypingStatus(false);
-    }
   };
 
   const renderItem = ({ item }) => (
@@ -131,90 +57,53 @@ const ChatScreen = ({ route }) => {
       ]}
     >
       <Text style={styles.messageText}>{item.text}</Text>
-      <View style={styles.messageMeta}>
-        <Text style={styles.messageTimestamp}>
-          {format(item.createdAt.toDate(), "HH:mm")}
-        </Text>
-        {item.read && item.userId === auth.currentUser.uid && (
-          <MaterialIcons name="done-all" size={16} color="#4CAF50" />
-        )}
-      </View>
     </View>
   );
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    >
-      <View style={styles.header}>
-        <Text style={styles.chatName}>{chatName}</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.chatName}>{chatName}</Text>
       <FlatList
         data={messages}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         style={styles.messageList}
-        ref={inputRef}
-        onContentSizeChange={() =>
-          inputRef.current.scrollToEnd({ animated: true })
-        }
       />
-      {otherUserTyping && (
-        <Text style={styles.typingIndicator}>The other user is typing...</Text>
-      )}
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
           value={newMessage}
-          onChangeText={handleTyping}
+          onChangeText={setNewMessage}
           placeholder="Type a message"
-          placeholderTextColor="#888"
-          onSubmitEditing={handleSend}
         />
         <TouchableOpacity onPress={handleSend} style={styles.sendButton}>
           <MaterialIcons name="send" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f4f4f4",
-  },
-  header: {
-    padding: 16,
-    backgroundColor: "#24786D",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#fff",
   },
   chatName: {
     fontSize: 20,
     fontWeight: "bold",
-    color: "#fff",
+    margin: 16,
   },
   messageList: {
     flex: 1,
   },
   messageItem: {
-    padding: 12,
-    backgroundColor: "#fff",
-    borderRadius: 8,
+    padding: 10,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
     marginVertical: 4,
     marginHorizontal: 16,
     alignSelf: "flex-start",
-    maxWidth: "75%",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
   },
   myMessage: {
     backgroundColor: "#dcf8c6",
@@ -222,51 +111,27 @@ const styles = StyleSheet.create({
   },
   messageText: {
     fontSize: 16,
-    color: "#333",
-  },
-  messageMeta: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 4,
-  },
-  messageTimestamp: {
-    fontSize: 12,
-    color: "#888",
-    marginRight: 4,
   },
   inputContainer: {
     flexDirection: "row",
     padding: 16,
     borderTopWidth: 1,
     borderTopColor: "#ddd",
-    backgroundColor: "#fff",
-    alignItems: "center",
   },
   input: {
     flex: 1,
     borderWidth: 1,
     borderColor: "#ddd",
-    borderRadius: 20,
+    borderRadius: 5,
     padding: 10,
-    paddingLeft: 16,
-    fontSize: 16,
-    color: "#333",
-    backgroundColor: "#f9f9f9",
   },
   sendButton: {
     backgroundColor: "#24786D",
-    borderRadius: 20,
+    borderRadius: 5,
     padding: 10,
     marginLeft: 10,
     justifyContent: "center",
     alignItems: "center",
-  },
-  typingIndicator: {
-    paddingHorizontal: 16,
-    color: "#888",
-    fontStyle: "italic",
-    marginBottom: 5,
-    textAlign: "center",
   },
 });
 
