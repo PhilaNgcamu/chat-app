d542cd6613b1617b2229772c115c4d5ca3601428
PhilaNgcamu <philangcamu17@gmail.com>
2024-06-04 23:28:38 +0200

    Created Group Chat

--- components/user_profile/UserProfile.js
+++ components/user_profile/UserProfile.js
@@ -7,6 +7,9 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  Dimensions,
+  ScrollView,
+  useWindowDimensions,
 } from "react-native";
 import { useFonts } from "expo-font";
 import { getAuth, updateProfile, updateEmail } from "firebase/auth";
@@ -15,6 +18,7 @@ import { getDatabase, ref, set, get } from "firebase/database";
 import * as ImagePicker from "expo-image-picker";
 import { useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { verticalScale } from "../../util/scale";
 
 const UserProfile = () => {
   const auth = getAuth();
@@ -26,6 +30,10 @@ const UserProfile = () => {
   const [profilePicture, setProfilePicture] = useState(user.photoURL || "");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [mediaShared, setMediaShared] = useState(0);
+
+  const { height } = useWindowDimensions();
+
+  const infoContainerHeight = height * 0.7;
 
   const [fontsLoaded] = useFonts({
     "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
@@ -88,7 +96,7 @@ const UserProfile = () => {
 
   if (!fontsLoaded) {
     return (
-      <View>
+      <View style={styles.loadingContainer}>
         <Text>Loading...</Text>
       </View>
     );
@@ -96,91 +104,132 @@ const UserProfile = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Edit Profile</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={pickImage}>
+            <Image
+              source={{ uri: profilePicture }}
+              style={styles.profilePicture}
+            />
+          </TouchableOpacity>
 
-      <TouchableOpacity onPress={pickImage}>
-        <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
-      </TouchableOpacity>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity>
+              <MaterialIcons name="message" size={24} color="#24786D" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MaterialIcons name="videocam" size={24} color="#24786D" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MaterialIcons name="call" size={24} color="#24786D" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <MaterialIcons name="more-vert" size={24} color="#24786D" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
 
-      <View style={styles.iconContainer}>
-        <TouchableOpacity>
-          <MaterialIcons name="message" size={24} color="#24786D" />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <MaterialIcons name="videocam" size={24} color="#24786D" />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <MaterialIcons name="call" size={24} color="#24786D" />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <MaterialIcons name="more-vert" size={24} color="#24786D" />
-        </TouchableOpacity>
+      <View style={styles.infoContainer}>
+        <View style={styles.dragger} />
+        <View style={styles.infoContent}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+              style={styles.input}
+              value={name}
+              onChangeText={setName}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Status Message</Text>
+            <TextInput
+              style={styles.input}
+              value={statusMessage}
+              onChangeText={setStatusMessage}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Phone Number</Text>
+            <TextInput
+              style={styles.input}
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              keyboardType="phone-pad"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Media Shared</Text>
+            <Text style={styles.input}>{mediaShared}</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleSave}>
+            <Text style={styles.buttonText}>Save Changes</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Name</Text>
-        <TextInput style={styles.input} value={name} onChangeText={setName} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Status Message</Text>
-        <TextInput
-          style={styles.input}
-          value={statusMessage}
-          onChangeText={setStatusMessage}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Phone Number</Text>
-        <TextInput
-          style={styles.input}
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-          keyboardType="phone-pad"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Media Shared</Text>
-        <Text style={styles.input}>{mediaShared}</Text>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={handleSave}>
-        <Text style={styles.buttonText}>Save Changes</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    padding: 16,
   },
-  title: {
-    textAlign: "center",
-    fontFamily: "Poppins-Bold",
-    fontSize: 24,
-    marginBottom: 16,
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerContainer: {
+    alignItems: "center",
+    marginTop: 25,
+    marginBottom: 30,
   },
   profilePicture: {
     width: 100,
     height: 100,
     borderRadius: 50,
-    alignSelf: "center",
     marginBottom: 16,
   },
   iconContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
+    width: "100%",
     marginBottom: 16,
+  },
+  infoContainer: {
+    position: "absolute",
+    width: "100%",
+    bottom: 0,
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    height: verticalScale(500),
+  },
+  dragger: {
+    alignSelf: "center",
+    backgroundColor: "#D3D3D3",
+    width: 40,
+    height: 5,
+    borderRadius: 5,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  infoContent: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
   },
   inputContainer: {
     marginBottom: 16,
