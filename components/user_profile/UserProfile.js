1a819111ccaa537d3693e4f7e030aa73e7759efb
PhilaNgcamu <philangcamu17@gmail.com>
2024-06-04 23:33:41 +0200

    responsiveness

--- components/user_profile/UserProfile.js
+++ components/user_profile/UserProfile.js
@@ -201,7 +201,7 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    marginBottom: 16,
+    marginBottom: 25,
   },
   iconContainer: {
     flexDirection: "row",
