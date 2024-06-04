ebb8700e4402ed2ed95d6c22fc18f052b9e20ff6
PhilaNgcamu <philangcamu17@gmail.com>
2024-06-04 09:57:05 +0200

    Chaatted between users

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
-import ChatList from "./components/chat/ChatList";
-import UserLogin from "./components/onboarding/UserLogin";
-import UserSignup from "./components/onboarding/UserSignup";
 import NavigationScreens from "./components/screens/Navigation";
-import UserProfile from "./components/userProfile/userProfile";
 
 export default function App() {
   return <NavigationScreens />;
